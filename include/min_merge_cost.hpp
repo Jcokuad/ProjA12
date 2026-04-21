8d85aebed38e0fb6228909026988807dde181bfc
Joshua O. <156972833+Jcokuad@users.noreply.github.com>
2026-04-21 12:12:01 -0400

    Problem 1 implementation

--- include/min_merge_cost.hpp
+++ include/min_merge_cost.hpp
@@ -1,5 +1,39 @@
 #pragma once
 
-#include <queue>
 #include <vector>
+#include "heap_implementation.hpp"
+
+namespace dsac::priority {
+
+int min_merge_cost(const std::vector<int>& files) {
+    // Check if there are 1 or less files
+    if (files.size() <= 1) {
+        return 0;
+    }
+
+    // Initialize a min-heap
+    HeapPriorityQueue<int> hq;
+
+    // add all files to the heap
+    for (int file_sz : files) {
+        hq.insert(file_sz);
+    }
+
+    int total = 0;
+    while (hq.size() > 1) {
+        int first{hq.min()}; // assign the first smallest to first, then remove from heap
+        hq.remove_min();
+
+        int second{hq.min()}; // assign the next smallest to second, then remove from heap
+        hq.remove_min();
+
+        int merge = first + second; // cost of the merge is a + b
+        total += merge;
+        hq.insert(merge); // put the merged file back in the heap
+    }
+
+    return total;
+}
+
+}
 
